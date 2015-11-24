f34f668c44d724541fb14b6dfccefb71b5312b7c
st585 <st585@cornell.edu>
2015-11-24 15:34:47 -0500

    compiling version of monster.mli

--- monster.mli
+++ monster.mli
@@ -1,17 +1,18 @@
-open constants
+open Constants
+open Dijkstra
 
 (** define variant type move_type: chasing, random. This designates whether
  ** whether the monster is moving randomly or actively chasing the player **)
-type move_type: Chasing | Random
+type move_type= Chasing | Random
 
 (** type wait_left is the duration of time left before the monster can move
- ** again **)
-type wait_left: ref int
+ ** again - is an int ref, defined in .ml **)
 
 (** decision function that takes in move_type and time left and returns the
  ** type of the next move done by the monster- helper for update
-val decision: move_type -> int -> move_type **)
+val decision: unit -> unit **)
 
 (** a function update_monster_position that takes in current monster position,
  ** player position, move type, and returns a new monster position **)
-val update_monster_position: position -> position -> int -> levels_board -> position
+val update_monster_position: position -> position -> int ->
+                             levels_board -> position
