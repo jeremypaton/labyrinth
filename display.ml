c217bf4fe2f4344a261c7531765959bba2e83ecc
nicolaskdc <nicolaskdc@yahoo.fr>
2015-11-28 18:25:11 -0500

    changing levels works

--- display.ml
+++ display.ml
@@ -10,7 +10,7 @@ type box_data = { x:int; y:int; w:int; h:int; bw:int; mutable r:relief;
 (* stores info about the displayed image, including number of possible x and
  * y positions, the scaling of these positions for viewability, and the current
  * x and y positions of the mouse *)
-type display = {grid:box_data array; maxx:int; maxy:int; x_sep:int; y_sep:int;
+type display = {mutable grid:box_data array; maxx:int; maxy:int; x_sep:int; y_sep:int;
                 mutable x : int; mutable y :int; scale:int; bc : Graphics.color;
                 fc: Graphics.color; pc : Graphics.color}
 
@@ -104,7 +104,7 @@ let draw_player (s:display) (pos: int*int) =
   let border = 2. in
   let tempbcf = pos_to_boxdata s pos scaling border in
   let bcf = {tempbcf with c1=Graphics.black; c2=gray2; c3=gray3} in
-  Printf.printf "%s\n%!" ("player pos is "^(string_of_int (fst pos))^", "^(string_of_int (snd pos)));
+  (*Printf.printf "%s\n%!" ("player pos is "^(string_of_int (fst pos))^", "^(string_of_int (snd pos)));*)
   draw_block bcf
 
 let draw_monster (s:display) (pos: int*int) =
@@ -120,13 +120,12 @@ let draw_monster (s:display) (pos: int*int) =
 
 
 
-let skel (lvl:int) f_init f_loop f_end f_key f_mouse f_except =
-  f_init ();
+let skel (lvl:int) f_draw_board f_loop f_end f_key f_mouse f_except =
+  f_draw_board ();
   let game = ref (match (Constants.init_level lvl) with
                   | Some x -> x
                   | None -> failwith "display.ml : cannot access level") in
-  let pos = !game.player_position in
-  Printf.printf "%s\n%!" ("player pos is "^(string_of_int (fst pos))^", "^(string_of_int (snd pos)));
+  (*Printf.printf "%s\n%!" ("player pos is "^(string_of_int (fst pos))^", "^(string_of_int (snd pos)));*)
   try
     while true do
       (*try*)
@@ -134,7 +133,7 @@ let skel (lvl:int) f_init f_loop f_end f_key f_mouse f_except =
                                           Graphics.Key_pressed; Graphics.Poll] in
         if s.Graphics.keypressed then
           let k = Graphics.read_key () in
-          f_init (); f_loop game [k]
+          f_loop game [k]
         else if s.Graphics.button then
           f_mouse s.Graphics.mouse_x s.Graphics.mouse_y
         (*else
@@ -180,9 +179,8 @@ let draw_point x y s c =
 
 (* Initialize the board in the display (i.e. draw the grid). Player and
  * monsters are not drawn *)
-let t_init (s:display) () =
-  Graphics.open_graph (" " ^ (string_of_int (s.scale*s.maxx)) ^
-  "x" ^ (string_of_int (s.scale*s.maxy)));
+let t_draw_board (s':display ref) () =
+  let s = !s' in
   Graphics.set_color s.bc;
   Graphics.fill_rect 0 0 (s.scale*s.maxx+1) (s.scale*s.maxy+1);
   (*draw_point s.x s.y s.scale s.pc*)
@@ -197,38 +195,6 @@ let get_master_board lvl=
 let flip_y pos board=
   let rows = (List.length board) in
   ((rows-fst pos)-1, snd pos)
-
-(* Loop function to be called every frame and after each key press after
- * initialization. Updates player and monsters positions *)
-let t_loop (s:display) (game:game_state ref) (keys:char list) =
-  match keys with
-  (* exit program if 'e'/'E' is pressed *)
-  | 'e'::_ | 'E'::_ -> raise End
-  (* run the main update function on key input otherwise *)
-  | _ -> game := Update.main_update !game keys;
-         let board = get_master_board !game.level_number in
-         (* draw player at new position *)
-         let pp = flip_y !game.player_position board in
-         draw_player s pp;
-         (* draw monsters at new positions *)
-         for i = 0 to (List.length !game.monster_position)-1 do
-           let mp = flip_y (snd (List.nth !game.monster_position i)) board in
-           draw_monster s mp
-         done
-
-
-(* Exits the program *)
-let t_end s () =
-  Graphics.close_graph();
-  print_string "Game ended by user"; print_newline()
-
-
-(* Function to execute on mouse clicks *)
-let t_mouse s x y = ()
-
-
-(* Function to execute on exceptions, unless exceptions is [End], i.e close *)
-let t_except s ex = Printf.printf "%s\n%!" ("EXCEPTION IN DISPLAY")
 
 
 
@@ -255,33 +221,8 @@ let create_grid (s:display) (board: bool list list) (data:box_data) =
   !acc
 
 
-(*let rec create_grid' nb_col n sep (b:box_data) =
-  if n < 0 then []
-  else
-  let px = n mod nb_col and py = n / nb_col in
-  let nx = b.x +sep + px*(b.w+sep)
-  and ny = b.y +sep + py*(b.h+sep) in
-  let b1 = {b with x=nx; y=ny} in
-  b1 :: (create_grid' nb_col (n-1) sep b)*)
 
-
-
-let t_key (s:display) c =
-  draw_point s.x s.y s.scale s.fc;
-
-  (match c with
-  | '8' -> if s.y < s.maxy then s.y <- s.y + 1;
-  | '2' -> if s.y > 0 then s.y <- s.y - 1
-  | '4' -> if s.x > 0 then s.x <- s.x - 1
-  | '6' -> if s.x < s.maxx then s.x <- s.x + 1
-  | 'c' -> Graphics.set_color s.bc;
-  Graphics.fill_rect 0 0 (s.scale*s.maxx+1) (s.scale*s.maxy+1);
-  Graphics.clear_graph()
-  | 'e' -> raise End
-  | _ -> () );
-  draw_point s.x s.y s.scale s.pc
-
-let initialize_display board =
+let initialize_display (s':display ref) board =
   let maxx' = 1080 in
   let maxy' = 720 in
   let scale' = 1 in
@@ -296,18 +237,102 @@ let initialize_display board =
   let wall_data = {x=0; y=0; w=0;h=0; bw=5; c1=gray1; c2=gray3;
                    c3=gray2; r=Top} in
   let grid' = Array.of_list (create_grid temp_disp board wall_data) in
-  {temp_disp with grid=grid'}
+  s' := {temp_disp with grid=grid'}
 
 
-let slate (lvl:int) (disp:display) =
-  skel lvl (t_init disp) (t_loop disp) (t_end disp) (t_key disp)
+let start_level (s':display ref) lvl game =
+  game := (match (Constants.init_level lvl) with
+          | Some x -> x
+          | None -> failwith "display.ml : cannot access level");
+  initialize_display s' (get_master_board lvl)
+
+(* Loop function to be called every frame and after each key press after
+ * initialization. Updates player and monsters positions *)
+let t_loop (s':display ref) (game:game_state ref) (keys:char list) =
+  match keys with
+  (* exit program if 'e'/'E' is pressed *)
+  | 'e'::_ | 'E'::_ -> raise End
+  (* otherwise, run the main update function on key input *)
+  | _ -> (* call main update and change level if appropriate *)
+         let cur_lvl = !game.level_number in
+         game := Update.main_update !game keys;
+         if !game.level_number <> cur_lvl then
+           start_level s' !game.level_number game;
+         (* do all drawing on screen *)
+         t_draw_board s' ();
+         let board = get_master_board !game.level_number in
+         (* draw player at new position *)
+         let pp = flip_y !game.player_position board in
+         draw_player !s' pp;
+         (* draw monsters at new positions *)
+         for i = 0 to (List.length !game.monster_position)-1 do
+           let mp = flip_y (snd (List.nth !game.monster_position i)) board in
+           draw_monster !s' mp
+         done
+
+
+(* Exits the program *)
+let t_end s () =
+  Graphics.close_graph();
+  print_string "Game ended by user"; print_newline()
+
+
+(* Function to execute on mouse clicks *)
+let t_mouse s x y = ()
+
+
+(* Function to execute on exceptions, unless exceptions is [End], i.e close *)
+let t_except s ex = Printf.printf "%s\n%!" ("EXCEPTION IN DISPLAY")
+
+
+
+
+
+(*let rec create_grid' nb_col n sep (b:box_data) =
+  if n < 0 then []
+  else
+  let px = n mod nb_col and py = n / nb_col in
+  let nx = b.x +sep + px*(b.w+sep)
+  and ny = b.y +sep + py*(b.h+sep) in
+  let b1 = {b with x=nx; y=ny} in
+  b1 :: (create_grid' nb_col (n-1) sep b)*)
+
+
+
+let t_key (s':display ref) c =
+  let s = !s' in
+  draw_point s.x s.y s.scale s.fc;
+
+  (match c with
+  | '8' -> if s.y < s.maxy then s.y <- s.y + 1;
+  | '2' -> if s.y > 0 then s.y <- s.y - 1
+  | '4' -> if s.x > 0 then s.x <- s.x - 1
+  | '6' -> if s.x < s.maxx then s.x <- s.x + 1
+  | 'c' -> Graphics.set_color s.bc;
+  Graphics.fill_rect 0 0 (s.scale*s.maxx+1) (s.scale*s.maxy+1);
+  Graphics.clear_graph()
+  | 'e' -> raise End
+  | _ -> () );
+  draw_point s.x s.y s.scale s.pc
+
+
+
+let slate (lvl:int) (disp:display ref) =
+  let s = !disp in
+  Graphics.open_graph (" " ^ (string_of_int (s.scale*s.maxx)) ^
+  "x" ^ (string_of_int (s.scale*s.maxy)));
+  skel lvl (t_draw_board disp) (t_loop disp) (t_end disp) (t_key disp)
   (t_mouse disp) (t_except disp)
 
 
 
 
-let start_level lvl =
+let launch_game lvl =
   let board= get_master_board lvl in
-  slate lvl (initialize_display board)
+  let temp_disp = ref {grid=[||]; maxx=100; maxy=100; x=60; y=60; scale=1;
+                   bc=Graphics.rgb 130 130 130; fc=Graphics.black;
+                   pc=Graphics.red; x_sep=1; y_sep=1} in
+  initialize_display temp_disp board;
+  slate lvl temp_disp
 
-let _ = start_level 2
+let _ = launch_game 2
