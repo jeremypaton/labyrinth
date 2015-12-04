b2f5965e1eca4c81f6a8235f97f3c978a4fb6860
jeremypaton <jmp377@cornell.edu>
2015-12-03 20:32:49 -0500

    cleanup

--- player.mli
+++ player.mli
@@ -1,14 +1,7 @@
 open Constants
 open Input
 
-(** the player module updates the player location based on keypresses **)
-
-(** a key is a char representing a key pressed by the keyboard **)
-type key= char
-
-(** function update_player_position takes in player position, movement
- ** direction, global master board, and returns a new player position **)
-val update_player_position: position  ->key list ->  master_board ->
+(**moves player based on keypresses**)
+val update_player_position: position  ->char list ->  master_board ->
                             position
 
-(** takes in input key_list from update **)
